92b0d7045f499e3011def8761b2f12cd1c42b8e7
kkboy <b96505036@ntu.edu.tw>
2015-09-04 15:29:38 +0800

    launch a t2.micro server
    specify image-id, instance type and security_group

--- helloWorld.py
+++ helloWorld.py
@@ -10,6 +10,12 @@ with open('D:/AWS/automa/credentials.csv', 'rb') as csvFile:
         aws_access_key_id = row[0]
         aws_secret_access_key = row[1]
 
-conn = boto.ec2.connect_to_region("us-west-2", aws_access_key_id=aws_access_key_id,
+# connect to us-west-2 with key pairs which is stored in local
+conn = boto.ec2.connect_to_region("us-west-2",
+                                  aws_access_key_id=aws_access_key_id,
                                   aws_secret_access_key=aws_secret_access_key)
 
+conn.run_instances('ami-d5c5d1e5',
+                   key_name='kkboy_ec2',
+                   instance_type='t2.micro',
+                   security_groups=['kkboy_sg'])

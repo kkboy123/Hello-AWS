dad648de9775bd0f9888ea01c17305578388832b
kkboy <b96505036@ntu.edu.tw>
2015-09-13 02:30:02 +0800

    ingress & engress
    remove default rule

---
+++ config/security_group_conf.py
@@ -0,0 +1,27 @@
+__author__ = 'kkboy'
+
+import collections
+
+SecurityGroupRule = collections.namedtuple("SecurityGroupRule", ["ip_protocol", "from_port", "to_port", "cidr_ip"])
+
+CASSANDRA_RULES = [
+    SecurityGroupRule("tcp", "22", "22", "0.0.0.0/0"),
+    SecurityGroupRule("tcp", "1024", "65535", "0.0.0.0/0"),
+    SecurityGroupRule("tcp", "7000", "7000", "0.0.0.0/0",),
+    SecurityGroupRule("tcp", "61620", "61621", "0.0.0.0/0"),
+    SecurityGroupRule("tcp", "7199", "7199", "0.0.0.0/0",),
+    SecurityGroupRule("tcp", "8888", "8888", "0.0.0.0/0",),
+    SecurityGroupRule("tcp", "8983", "8983", "0.0.0.0/0",),
+    SecurityGroupRule("tcp", "9160", "9160", "0.0.0.0/0",),
+]
+
+TEST_RULES = [
+    SecurityGroupRule("tcp", "22", "22", "0.0.0.0/0"),
+]
+
+SecurityGroup = collections.namedtuple("SecurityGroup", ["security_group_name", "description", "rules_in", "rules_out"])
+
+SECURITY_GROUPS = [
+    SecurityGroup("Cassandra Cluster", "Cassandra Cluster description", CASSANDRA_RULES, CASSANDRA_RULES),
+    SecurityGroup("Test", "Test description", TEST_RULES, TEST_RULES),
+]
--- tests/test_SG.py
+++ tests/test_SG.py
@@ -4,26 +4,80 @@ from utils.auth import aws_access_key_id, aws_secret_access_key
 from boto.ec2.securitygroup import SecurityGroup
 from boto.ec2 import EC2Connection
 import boto.vpc
-from boto.vpc import VPCConnection
-import collections
+from config.security_group_conf import SECURITY_GROUPS
 
 
-# # VPC connection
-# conn = boto.vpc.connect_to_region("us-west-2",
-#                                aws_access_key_id=aws_access_key_id,
-#                                aws_secret_access_key=aws_secret_access_key)
-#
-#
-# #def create_security_group():
+
+# VPC connection
+conn = boto.ec2.connect_to_region("us-west-2",
+                               aws_access_key_id=aws_access_key_id,
+                               aws_secret_access_key=aws_secret_access_key)
+
 # name = "kk_sg1"
 # description = "testing sg1"
 # vpc_id = "vpc-f76e2b92"
+for security_group in SECURITY_GROUPS:
+    sg = conn.create_security_group(security_group.security_group_name,
+                                    security_group.description,
+                                    vpc_id="vpc-f76e2b92")
+    print sg.id
+    for rule in security_group.rules_in:
+        sg.authorize(rule.ip_protocol,
+                     rule.from_port,
+                     rule.to_port,
+                     rule.cidr_ip)
+    print "rules_in is done"
+    # remove default rule
+    conn.revoke_security_group_egress(sg.id,
+                                      u"-1",
+                                      from_port=None,
+                                      to_port=None,
+                                      cidr_ip="0.0.0.0/0")
+    for rule in security_group.rules_out:
+        conn.authorize_security_group_egress(group_id=sg.id,
+                                             ip_protocol=rule.ip_protocol,
+                                             from_port=rule.from_port,
+                                             to_port=rule.to_port,
+                                             cidr_ip=rule.cidr_ip)
+    print "%s is done" % security_group.security_group_name
+
+
 # sg = conn.create_security_group(name, description, vpc_id = "vpc-f76e2b92")
 # print sg
 # print sg.authorize('tcp', 80, 80, '0.0.0.0/0')
+# sg_id="sg-7e17961a"
+# print conn.authorize_security_group_egress(group_id=sg_id,
+#                                            ip_protocol="tcp",
+#                                            from_port=80,
+#                                            to_port=80,
+#                                            cidr_ip="0.0.0.0/0")
+
+# for security_group in SECURITY_GROUPS:
+#     print security_group.security_group_name
+#     print security_group.description
+#     for rule in security_group.rules:
+#         print "rule-------------"
+#         print rule.cidr_ip
+#         print rule.from_port
+#         print rule.to_port
+#         print rule.ip_protocol
+#         print "rule-------------"
+#     print "============"
+
+
+
+
+
+
+
+
+
+
+
 
 
 # security attributes
+#
 # rs = conn.get_all_security_groups()
 # sg = rs[1]
 # print "name : %s" % sg.name
@@ -32,37 +86,10 @@ import collections
 # print "description : %s" % sg.description
 # print "vpc_id : %s" % sg.vpc_id
 # print "rules : %s" % sg.rules
-# print "rules_egress : %s" % sg.rules_egress
+# #print "rules_egress : %s" % sg.rules_egress
+# for rule in sg.rules_egress:
+#     print type(u"-1")
+#     print rule.from_port
+#     print rule.to_port
+#     print rule.grants[0].cidr_ip
 
-SecurityGroupRule = collections.namedtuple("SecurityGroupRule", ["ip_protocol", "from_port", "to_port", "cidr_ip", "src_group_name"])
-
-CASSANDRA_RULES = [
-    SecurityGroupRule("tcp", "22", "22", "0.0.0.0/0", None),
-    SecurityGroupRule("tcp", "1024", "65535", "0.0.0.0/0", "Cassandra Cluster"),
-    SecurityGroupRule("tcp", "7000", "7000", "0.0.0.0/0", "Cassandra Cluster"),
-    SecurityGroupRule("tcp", "61620", "61621", "0.0.0.0/0", "Cassandra Cluster"),
-    SecurityGroupRule("tcp", "7199", "7199", "0.0.0.0/0", None),
-    SecurityGroupRule("tcp", "8888", "8888", "0.0.0.0/0", None),
-    SecurityGroupRule("tcp", "8983", "8983", "0.0.0.0/0", None),
-    SecurityGroupRule("tcp", "9160", "9160", "0.0.0.0/0", None),
-]
-
-TEST_RULES = [
-    # ssh makes life possible
-    SecurityGroupRule("tcp", "22", "22", "0.0.0.0/0", None),
-]
-
-SECURITY_GROUPS = [("Cassandra Cluster", CASSANDRA_RULES),
-                   ("Test", TEST_RULES)
-                   ]
-# for group_name, rules in SECURITY_GROUPS:
-#     print group_name
-#     for rule in rules:
-#         print "rule-------------"
-#         print rule.cidr_ip
-#         print rule.from_port
-#         print rule.to_port
-#         print rule.ip_protocol
-#         print rule.src_group_name
-#         print "rule-------------"
-#     print "============"
